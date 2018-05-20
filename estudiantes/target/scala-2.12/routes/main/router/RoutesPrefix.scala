6cb7dd478adefb39b30d7e47dcf137e59c971784
1213lego <36804989+1213lego@users.noreply.github.com>
2018-05-19 20:46:30 -0500

    listando estudiantes

--- estudiantes/target/scala-2.12/routes/main/router/RoutesPrefix.scala
+++ estudiantes/target/scala-2.12/routes/main/router/RoutesPrefix.scala
@@ -1,6 +1,6 @@
 // @GENERATOR:play-routes-compiler
-// @SOURCE:C:/Users/ss/Desktop/Estructura de datos/estudiantes/conf/routes
-// @DATE:Fri May 18 16:45:46 COT 2018
+// @SOURCE:C:/Users/ss/Desktop/Estructura de datos/sbt/estudiantes/conf/routes
+// @DATE:Sat May 19 20:44:27 COT 2018
 
 
 package router {
